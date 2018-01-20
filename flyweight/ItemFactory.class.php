571c6a5a36f42af63a077571104e195f2cb94f17
Mitsuru Hashimoto <exhikkii@gmail.com>
2018-01-20 10:16:11 +0900

    flyweight

---
+++ flyweight/ItemFactory.class.php
@@ -0,0 +1,47 @@
+<?php
+require_once 'Item.class.php';
+
+class ItemFactory {
+  private $pool;
+  private static $instance = null;
+
+  public function __construct($filename)
+  {
+    $this->buildPool($filename);
+  }
+
+  public static function getInstance($filename)
+  {
+    if (is_null(self::$instance)) {
+      self::$instance = new ItemFactory($filename);
+    }
+    return self::$instance;
+  }
+
+  public function getItem($code)
+  {
+    if (array_key_exists($code, $this->pool)) {
+      return $this->pool[$code];
+    } else {
+      return null;
+    }
+  }
+
+  private function buildPool($filename)
+  {
+    $this->pool = [];
+
+    $fp = fopen($filename, 'r');
+    while ($buffer = fgets($fp, 4096)) {
+      list($item_code, $item_name, $price) = split("\t", $buffer);
+      $this->pool[$item_code] = new Item($item_code, $item_name, $price);
+    }
+    fclose($fp);
+  }
+
+  public final function __clone()
+  {
+    throw new RuntimeException('Clone is not allowed against ' . get_class($this));
+  }
+}
+ ?>
